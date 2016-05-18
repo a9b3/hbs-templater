91133277edfdbd94e0a747cd51253d21ac85ac49
Sam Lau <esayemm@gmail.com>
2016-05-18 02:51:34 -0700

    rough draft

---
+++ src/api.js
@@ -0,0 +1,87 @@
+const packageJson = require('../package.json')
+import chalk from 'chalk'
+import path from 'path'
+import fs from 'fs'
+import handlebars from 'handlebars'
+import { sync as rmrf } from 'rimraf'
+import { sync as mkdirp } from 'mkdirp'
+
+export function fileExists(file) {
+  try {
+    fs.accessSync(file, fs.F_OK)
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+function recursiveDirectory(input, fn) {
+  const isDirectory = fs.lstatSync(input).isDirectory()
+
+  if (isDirectory) {
+    const files = fs.readdirSync(input)
+    files.forEach(file => {
+      const filePath = path.resolve(input, file)
+      recursiveDirectory(filePath, fn)
+    })
+  } else {
+    fn(input)
+  }
+}
+
+function compileTemplate({
+  templateFilePath,
+  outputFilePath,
+  params,
+  log = false,
+} = {}) {
+  const templateFile = fs.readFileSync(templateFilePath, 'utf8')
+  const compile = handlebars.compile(templateFile)
+  const rendered = compile(params)
+
+  if (fileExists(outputFilePath)) {
+    log && console.log(chalk.yellow(`File already exists ${outputFilePath}`))
+    return
+  }
+
+  log && console.log(chalk.green(`Creating the file at ${outputFilePath}`))
+
+  mkdirp(path.dirname(outputFilePath))
+  fs.writeFileSync(outputFilePath, rendered)
+}
+
+export function compile({
+  params = {},
+  input,
+  output,
+  log = false,
+} = {}) {
+
+  recursiveDirectory(input, (filePath) => {
+    const pathDiff = filePath.replace(input + '/', '')
+    const outputFilePath = path.resolve(output, pathDiff)
+
+    compileTemplate({
+      templateFilePath: filePath,
+      outputFilePath,
+      params,
+      log,
+    })
+  })
+}
+
+export function help() {
+  console.log(``)
+  console.log(`  ${packageJson.name} ${packageJson.version}`)
+  console.log(``)
+  console.log(`  compile`)
+  console.log(`    --[params | p]`)
+  console.log(`      json string or json file location or js module export file location`)
+  console.log(`    --[input | i]`)
+  console.log(`      template input file path can be directory or file`)
+  console.log(`    --[output | o]`)
+  console.log(`      ouput directory`)
+  console.log(`    --[log | l]`)
+  console.log(`      log info`)
+  console.log(``)
+}
---
+++ src/command-handler.js
@@ -0,0 +1,60 @@
+import process from 'process'
+import chalk from 'chalk'
+import path from 'path'
+import {
+  compile,
+  help,
+  fileExists,
+} from './api.js'
+
+function getParams() {
+  const argv = require('yargs').argv
+  const arg = argv.params || argv.p
+
+  if (/\.(js|json)$/.test(arg)) {
+    const filePath = path.resolve('.', arg)
+    if (!fileExists(filePath)) {
+      console.log(chalk.red(`Not valid params path.`))
+      process.exit(-1)
+    }
+    return require(filePath)
+  } else {
+    try {
+      const parsed = JSON.parse(arg)
+      return parsed
+    } catch (e) {
+      console.log(chalk.red(`Not valid params must be json format.`))
+      process.exit(-1)
+    }
+  }
+}
+
+export default function commandHandler() {
+  const argv = require('yargs').argv
+  const command = argv._[0]
+
+  switch(command) {
+    case 'compile':
+    case 'c':
+      const params = getParams()
+      const input = path.resolve('.', argv.input || argv.i)
+      const output = path.resolve('.', argv.output || argv.o)
+      const log = Boolean(argv.log || argv.l)
+
+      if (!fileExists(input)) {
+        console.log(chalk.red(`Input file path is not valid.`))
+        process.exit(-1)
+      }
+
+      compile({
+        params,
+        input,
+        output,
+        log,
+      })
+      break
+    default:
+      help()
+      return
+  }
+}
